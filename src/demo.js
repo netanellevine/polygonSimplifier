9d9a2633ca3e176ea5a4c8c64bdb218e7b7b1880
Your Name <you@example.com>
2023-01-05 15:35:26 +0200

    updated using simplify

--- src/demo.js
+++ src/demo.js
@@ -1,9 +1,9 @@
-const transformScale = require ('@turf/transform-scale');
+const turf = require ('@turf/turf');
 const fs = require("fs");
 
 
 const PATH = '../data';
-const file_name = 'Canada_polygon.geojson';
+const file_name = 'australia.geojson';
 const country_name = file_name.split('_')[0].toLowerCase();
 const exmp_dir = 'examples/scaled';
 
@@ -12,14 +12,34 @@ const data = JSON.parse(json);
 console.log(data.features[0].geometry.coordinates[0].length);
 // console.log(data.features[0].geometry.coordinates[0]);
 
-const factor = 1.1
-var ans = transformScale(data, factor, {origin: 'center'});
-console.log(ans.features[0].geometry.coordinates[0].length);
-// console.log(ans.features[0].geometry.coordinates[0]);
+const simplifier = {
+    maxPoints: 500,
+    highQuality: true,
+    mutate: true,
+    tolerance: {
+        1000: 0.01,
+        10000: 0.1,
+        100000: 1
+    },
+    precision: {
+        999999999: 6,
+        99999999999: 5,
+        999999999999: 4,
+        9999999999999: 3,
+        99999999999999: 2,
+    },
+    levels: {
+        HIGH: 2,
+        MEDIUM: 1,
+        LOW: 0
+    }
+};
+const area = turf.area(data);
+console.log(file_name, area);
+let t = Object.keys(simplifier.precision).find(key => area < key);
+console.log(simplifier.precision[t]);
 
-const s = (String(factor)).replace('.', '');
-const new_file_name = `scaled_${country_name}_s-${s}.geojson`;
-fs.writeFileSync(`${PATH}/${exmp_dir}/${new_file_name}`, JSON.stringify(ans));
+
 
 
 // combine
--- src/simplifier_convex.js
+++ src/simplifier_convex.js
@@ -13,10 +13,12 @@ const MAX_SIZE = 500;
 function simplifyShape(file, level=0) {
     // skip = -1 , low = default = 0, medium = 1, high = 2
     const file_name = file;
-    const country_name = file_name.split('_')[0].toLowerCase();
+    const country_name = file_name.split('.')[0].toLowerCase();
     const json =  fs.readFileSync(`${PATH}/${file_name}`);
     const data = JSON.parse(json);
     var concavity;
+    var tol = 0.5;
+    var quality = true;
 
     // In order to determine the concavity, we need to know the size of the shape.
     const before_length = coordAll(data).length;
@@ -25,67 +27,61 @@ function simplifyShape(file, level=0) {
     var cop = JSON.parse(JSON.stringify(data));
     // if type is MultyPolygon, we need to convert it to Polygons.
     if (data.features[0].geometry.type == 'MultiPolygon') {
-            simplifyMultyPolygon(cop);
+        let len = coordAll(data).length;
+        while (len > MAX_SIZE) {
+            // console.log("LEN", len);
+            // console.log("CONCAVITY", concavity);
+            // console.log("TOL", tol);
+            // console.log("QUALITY", quality);
+            simplifyMultyPolygon(cop, tol, quality);
+            len = coordAll(cop).length;
+            // tol *= 2;
+        }
+        // simplifyMultyPolygon(cop, tol, quality);
     }
     else {
         console.log("BEFORE: ", coordAll(cop).length);
         var options = {precision: 6};
         cop = turf.truncate(cop, options);
 
-        options = {tolerance: 0.1, highQuality: false, mutate: true};
+        options = {tolerance: tol, highQuality: quality, mutate: true};
         turf.simplify(cop, options);
 
         console.log("AFTER: ", coordAll(cop).length);
     }
 
-
-
-    // // OPTIONAL-> Remove redundent coordinates.
-    // const clear = cleanCoords(data.features[0]);
-    // console.log("AFTER cleanCoords ", coordAll(clear).length);
-
-    // // The concavity is determined by the size of the shape and the level of simplification.
-    // concavity = getConcavity(level, before_length);
-
-    // // Simplify the shape using the convex hull algorithm.
-    // var ans = convex(data, {'concavity': concavity});
-
     var after_length = coordAll(cop).length;
     console.log("AFTER ", after_length);
 
-    // // If the shape is still too big, we increase the concavity and try again.
-    // var jump = 2;
-    // while (after_length > MAX_SIZE) {
-    //     var ans = convex(ans, {'concavity': concavity});
-    //     concavity += jump;
-    //     jump -= 0.1;
-    //     after_length = coordAll(ans).length;
-    //     console.log(`AFTER concavity: ${concavity}, length: ${after_length}`);
-    // }
-
     // const c = (String(concavity)).replace('.', '');
-    fs.writeFileSync(`simplified_${country_name}_length-${after_length}.geojson`, JSON.stringify(cop));
+    fs.writeFileSync(`simplified_${country_name}_tol-${tol}_q-${quality}_length-${after_length}.geojson`, JSON.stringify(cop));
 }
 
 
 
-function simplifyMultyPolygon(data) {
+function simplifyMultyPolygon(data, tol, quality) {
     var length = data.features[0].geometry.coordinates.length;
-    console.log("LENGTH", length);
+    console.log("LENGTH BEFORE", coordAll(data).length);
+
+    // Iterate over each polygon in the multyPolygon.
     for (var i = 0; i < length; i++) {
 
+        // Create a polygon from the coordinates.
         var polygon = turf.polygon(data.features[0].geometry.coordinates[i]);
 
-        console.log("BEFORE: ", coordAll(polygon).length);
+        // console.log("BEFORE: ", coordAll(polygon).length);
 
+        // Simplify the coordinates precision and remove redundent coordinates.
         var options = {precision: 6, mutate: true};
         turf.truncate(polygon, options);
 
-        options = {tolerance: 0.1, highQuality: false, mutate: true};
+        // Simplify the shape of the polygon to decrease the number of coordinates.
+        options = {tolerance: tol, highQuality: quality, mutate: true};
         turf.simplify(polygon, options);
 
         data.features[0].geometry.coordinates[i] = polygon.geometry.coordinates;
     }
+    console.log("AFTER: ", coordAll(data).length);
 }
 
 
@@ -121,20 +117,23 @@ function getConcavity(level, size) {
 
 
 const l = 2;
-console.log('CANADA LONG------------------------------------------------------------');
-simplifyShape(file='canada.geojson', level=l);
-console.log('CANADA SHORT------------------------------------------------------------');
-simplifyShape(file='map_canada.geojson', level=l);
+// console.log('CANADA LONG------------------------------------------------------------');
+// simplifyShape(file='canada.geojson', level=l);
+// console.log('CANADA SHORT------------------------------------------------------------');
+// simplifyShape(file='map_canada.geojson', level=l);
 console.log('AUSTRALIA------------------------------------------------------------');
 simplifyShape(file='australia.geojson', level=l);
-console.log('SWITZERLAND------------------------------------------------------------');
-simplifyShape(file='switzerland.geojson', level=l);
-console.log('COLOMBIA------------------------------------------------------------');
-simplifyShape(file='colombia.geojson', level=l);
-console.log('UK------------------------------------------------------------');
-simplifyShape(file='uk.geojson', level=l);
-console.log('JAPAN------------------------------------------------------------');
-simplifyShape(file='japan.geojson', level=l);
-console.log('RUSSIA------------------------------------------------------------');
-simplifyShape(file='russia.geojson', level=l);
+// console.log('SWITZERLAND------------------------------------------------------------');
+// simplifyShape(file='switzerland.geojson', level=l);
+// console.log('COLOMBIA------------------------------------------------------------');
+// simplifyShape(file='colombia.geojson', level=l);
+// console.log('UK------------------------------------------------------------');
+// simplifyShape(file='uk.geojson', level=l);
+// console.log('JAPAN------------------------------------------------------------');
+// simplifyShape(file='japan.geojson', level=l);
+// console.log('RUSSIA------------------------------------------------------------');
+// simplifyShape(file='russia.geojson', level=l);
+
+
+
 module.exports = {simplifyShape};
